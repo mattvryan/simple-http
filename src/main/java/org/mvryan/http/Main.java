d58b263d71f2f2f150401f1d63031a7758e9f87c
Matt Ryan <matt@mvryan.org>
2015-03-28 11:46:02 -0600

    Adds running "main" which does nothing except print status to console.

---
+++ src/main/java/org/mvryan/http/Main.java
@@ -0,0 +1,14 @@
+package org.mvryan.http;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main
+{
+    public static void main(String[] args)
+    {
+        log.info("Server started.");
+        
+        log.info("Server stopped.");
+    }
+}
